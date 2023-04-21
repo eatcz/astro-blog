410b33b4f8b74d88791cf1b42d545836fd9895c1
eatcz <zfcstdio@163.com>
2023-04-21 18:03:31 +0800

    添加文章页

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import react from "@astrojs/react";
-
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), preact(), react()]
+  integrations: [mdx(), tailwind(), preact(), react()],
+  markdown: {
+    rehypePlugins: [remarkReadingTime],
+  },
 });
---
+++ src/pages/posts/markdown.md
@@ -0,0 +1,17 @@
+---
+layout: '../../layouts/PostLayout.astro'
+title: '小记'
+pubDate: 2023-04-21,
+description: 'this blog is preliminary completion'
+author: 'Astro Learner'
+image:
+    url: 'https://p.qqan.com/up/2023-4/20234111348285347.jpg' 
+    alt: 'The Astro logo with the word One.'
+tags: ["astro", "blogging", "learning in public"]
+---
+
+#### 博客初步完成记录
+
+- 经过这几天的不懈奋斗，终于初步完成了这个博客,qwq!
+- 博客采用了Astro + React + MDX 进行搭建，也算是学习Astro的一次实战吧。
+- 话少，不知道说啥了， 那就再见吧！
